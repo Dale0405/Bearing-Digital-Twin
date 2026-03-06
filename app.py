a757be45011dde61930d735d277e7457bac63f6f
Dale0405 <nombredaleosorio05@gmail.com>
2026-03-06 10:29:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.title("Bearing Digital Twin")
+st.title("BEARING TESTING DIGITAL TWIN")
 
 # ----------------------------
 # Bearing Parameters
