4112a79a98c20b95ed465efb5adf2f053748b227
Dale0405 <nombredaleosorio05@gmail.com>
2026-03-09 13:43:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -580,12 +580,9 @@ if "Vibration (g)" in data_table:
 # Display table
 st.subheader("Test Data Table")
 
-styled_table = data_table.style.set_table_styles(
-    [
-        {"selector": "th", "props": [("text-align", "center")]},
-        {"selector": "td", "props": [("text-align", "center")]}
-    ]
-)
+styled_table = data_table.style.set_properties(**{
+    'text-align': 'center'
+})
 
 st.dataframe(styled_table, use_container_width=True)
 
