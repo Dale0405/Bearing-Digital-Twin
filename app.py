0a9f6467b38557629cbbd5e495bccb4608dc0391
Dale0405 <nombredaleosorio05@gmail.com>
2026-03-14 16:36:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1196,76 +1196,76 @@ if page == "Test Results":
     # RESULTS TABLE LAYOUT
     # ----------------------------
 
-    with st.expander("Test Results Summary", expanded=True):
+with st.expander("Test Results Summary", expanded=True):
         
-        st.markdown(f"""
-        <table style="
-        width:{table_width}%;
-        border-collapse:collapse;
-        text-align:center;
-        margin:auto;
-        font-size:{text_size}px;
-        ">
-        
-        <tr>
-        <th colspan="3" style="
-        border:1px solid white;
-        padding:{table_height/3}px;
-        font-size:{header_text_size}px;
-        ">
-        Test Consistency
-        </th>
-        </tr>
-        
-        <tr>
-        <td style="border:1px solid white; height:{table_height}px;">Speed Variation</td>
-        <td style="border:1px solid white; height:{table_height}px;">Load Variation</td>
-        <td style="border:1px solid white; height:{table_height}px;">Temperature Stability</td>
-        </tr>
-        
-        <tr>
-        <td style="border:1px solid white; height:{table_height}px; font-weight:bold;">
-        {speed_var:.2f} %
-        </td>
-        
-        <td style="border:1px solid white; height:{table_height}px; font-weight:bold;">
-        {load_var:.2f} %
-        </td>
-        
-        <td style="border:1px solid white; height:{table_height}px; font-weight:bold;">
-        ± {temp_std:.2f} °C
-        </td>
-        </tr>
-        
-        <tr>
-        <th colspan="3" style="
-        border:1px solid white;
-        padding:{table_height/3}px;
-        font-size:{header_text_size}px;
-        ">
-        Bearing Life
-        </th>
-        </tr>
-        
-        <tr>
-        <td style="border:1px solid white; height:{table_height}px;">Ideal L10</td>
-        <td style="border:1px solid white; height:{table_height}px;">Actual L10</td>
-        <td style="border:1px solid white; height:{table_height}px;">Life Consumption</td>
-        </tr>
-        
-        <tr>
-        <td style="border:1px solid white; height:{table_height}px; font-weight:bold;">
-        {L10_hours_ideal:,.0f} hr
-        </td>
-        
-        <td style="border:1px solid white; height:{table_height}px; font-weight:bold;">
-        {L10_hours_actual:,.0f} hr
-        </td>
-        
-        <td style="border:1px solid white; height:{table_height}px; font-weight:bold;">
-        {life_consumption:.2f} %
-        </td>
-        </tr>
+    st.markdown(f"""
+    <table style="
+    width:{table_width}%;
+    border-collapse:collapse;
+    text-align:center;
+    margin:auto;
+    font-size:{text_size}px;
+    ">
+    
+    <tr>
+    <th colspan="3" style="
+    border:1px solid white;
+    padding:{table_height/3}px;
+    font-size:{header_text_size}px;
+    ">
+    Test Consistency
+    </th>
+    </tr>
+    
+    <tr>
+    <td style="border:1px solid white; height:{table_height}px;">Speed Variation</td>
+    <td style="border:1px solid white; height:{table_height}px;">Load Variation</td>
+    <td style="border:1px solid white; height:{table_height}px;">Temperature Stability</td>
+    </tr>
+    
+    <tr>
+    <td style="border:1px solid white; height:{table_height}px; font-weight:bold;">
+    {speed_var:.2f} %
+    </td>
+    
+    <td style="border:1px solid white; height:{table_height}px; font-weight:bold;">
+    {load_var:.2f} %
+    </td>
+    
+    <td style="border:1px solid white; height:{table_height}px; font-weight:bold;">
+    ± {temp_std:.2f} °C
+    </td>
+    </tr>
+    
+    <tr>
+    <th colspan="3" style="
+    border:1px solid white;
+    padding:{table_height/3}px;
+    font-size:{header_text_size}px;
+    ">
+    Bearing Life
+    </th>
+    </tr>
+    
+    <tr>
+    <td style="border:1px solid white; height:{table_height}px;">Ideal L10</td>
+    <td style="border:1px solid white; height:{table_height}px;">Actual L10</td>
+    <td style="border:1px solid white; height:{table_height}px;">Life Consumption</td>
+    </tr>
+    
+    <tr>
+    <td style="border:1px solid white; height:{table_height}px; font-weight:bold;">
+    {L10_hours_ideal:,.0f} hr
+    </td>
+    
+    <td style="border:1px solid white; height:{table_height}px; font-weight:bold;">
+    {L10_hours_actual:,.0f} hr
+    </td>
+    
+    <td style="border:1px solid white; height:{table_height}px; font-weight:bold;">
+    {life_consumption:.2f} %
+    </td>
+    </tr>
         
         </table>
         """, unsafe_allow_html=True)
